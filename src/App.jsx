87733f86596d99f83da1acdb2611dfc91176b119
Olalekan <lekanbiodun2@gmail.com>
2025-10-17 23:26:56 +0100

    Updates on Join

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import "./index.css";
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
-import JoinUs from "./Components/JoinUs";
+import JoinUs from "./Components/Join";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/joinus" element={<JoinUs />} />
+        <Route path="/join" element={<JoinUs />} />
       </Routes>
     </BrowserRouter>
   );
