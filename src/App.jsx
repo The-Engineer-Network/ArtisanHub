21d2027f00396d574560021085ca9fcb805ef48e
Succex7 <successoghenegare7@gmail.com>
2025-11-21 18:30:35 +0100

    contact file
    added the contact  us section

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Profile from "./Pages/ArtisanDashboard/profile";
 import Portfolio from "./Pages/ArtisanDashboard/portfolio";
 import Message from "./Pages/ArtisanDashboard/message";
 import Settings from "./Pages/ArtisanDashboard/settings";
+import Contact from "./Pages/ArtisanDashboard/contact";
 
 function App() {
   return (
@@ -19,6 +20,8 @@ function App() {
         {/* Public Pages */}
         <Route path="/" element={<HomePage />} />
         <Route path="/join" element={<Join />} />
+        {/* Public contact page (allow visiting /contact directly) */}
+        <Route path="/contact" element={<Contact />} />
 
         {/* Artisan Dashboard (Nested routes inside layout) */}
         <Route path="/dashboard" element={<DashboardLayout />}>
@@ -27,6 +30,7 @@ function App() {
           <Route path="portfolio" element={<Portfolio />} />
           <Route path="messages" element={<Message />} />
           <Route path="settings" element={<Settings />} />
+          <Route path="contact" element={<Contact />} />
         </Route>
       </Routes>
     </BrowserRouter>
