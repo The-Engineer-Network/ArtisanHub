9e77b26a26d89013a840463f22136cd7f1f58e34
Aderemi67 <aderemiazeez6@gmail.com>
2026-01-08 07:53:25 +0100

    Revert "Updated About.jsx, package-lockson.json"
    This reverts commit 804f3a126e7ff3ffd43bd3b5f2e7b979ea87cccf.

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -154,7 +154,7 @@ const About = () => {
                             <img
                                 src={member.image}
                                 alt={member.name}
-                                className="w-full h-64"
+                                className="w-full h-64 object-cover"
                             />
                             <div className="p-6 text-center">
                                 <h3 className="text-xl font-aladin text-gray-800 font-bold">
